04a2788d9ef6fa653d62f6d47ccfd8e9daa2124d
etiiizel <franciscoetizel@gmail.com>
2025-07-15 17:02:46 -0300

    timestamp

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -1,6 +1,11 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
+import { create } from "domain";
+import { name } from "drizzle-orm";
+import { pgTable, PgTimestamp, text, timestamp, uuid } from "drizzle-orm/pg-core";
 
 export const rooms = pgTable('rooms', {
   id: uuid().primaryKey().defaultRandom(),
+  name: text().notNull(),
+  description: text(),
+  createdAt: timestamp().notNull().defaultNow(),
 
 })
