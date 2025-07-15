659660d00dc526296d17d764dc5ab979c7a1b8a9
etiiizel <franciscoetizel@gmail.com>
2025-07-15 16:20:14 -0300

    drizzle_orm-and-drizzle_kit

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,8 @@
 import postgres from 'postgres';
 import { env } from '../env.ts';
+// This establishes a connection to the PostgreSQL database using the URL from the environment variables.
 
 export const sql = postgres(env.DATABASE_URL)
 
-const result = await sql`SELECT 'Hello' as message`
-console.log(result)
+
+
