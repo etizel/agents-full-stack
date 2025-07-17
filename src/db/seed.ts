2a9efea01e519d7701667ed402cbcc814285a795
etiiizel <franciscoetizel@gmail.com>
2025-07-17 16:41:41 -0300

    routes-for-consume_api

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,5 +2,19 @@ import { reset, seed } from 'drizzle-seed'
 import { schema } from './schema/index.ts'
 import { db } from './connection.ts'
 
+
 await reset(db, schema)
-await seed(db, schema)
+await seed(db, schema).refine(f => {
+  return {
+    rooms: {
+      count: 20,
+      columns:{
+        name: f.companyName(),
+        description: f.loremIpsum(),
+      }
+    },
+  }
+})
+
+// biome-ignore lint/suspicious/noConsole: <only use for dev>
+console.log('Database seeded successfully.')
