6aea33c3f25ab090e32d1f6e868e51d278294824
etiiizel <franciscoetizel@gmail.com>
2025-07-18 16:14:23 -0300

    consuming-api_with-routes

--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -2,8 +2,10 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
  
-export const = getRoomsRoute: FastifyPluginAsyncZod = (app) => {
+export const getRoomsRoute: FastifyPluginAsyncZod = async (app) => {
   app.get('/rooms', async()  => {
     const result = await db.select().from(schema.rooms).orderBy(schema.rooms.createdAt)
+
+    return result
   } )
 }
